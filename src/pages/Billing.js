1d60679ecd59539044ca65c3078568f03c740f64
Jenish Limbu <jlimbu539@gmail.com>
2024-01-23 17:17:51 +0545

    billing content added

--- src/pages/Billing.js
+++ src/pages/Billing.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
+import BillingContent from "../components/BillingContent";
 const Billing = () => {
   return (
     <div className="body-color ">
-      <div className="row container px-4 py-4">
+      <div className="row g-0 px-4 py-4">
         <div
           style={{ width: "22%" }}
           className=" text-light h-100vh bg-sidebar-color rounded-4"
         >
           <Sidebar />
         </div>
-        <div className="col-9 text-light">Billings</div>
+        <div className="col-9 text-light">
+          <BillingContent />
+        </div>
       </div>
     </div>
   );
