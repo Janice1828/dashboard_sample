0e516b3a4efd621f03910baa314c08d1e5db9110
Jenish Limbu <jlimbu539@gmail.com>
2024-02-03 16:46:22 +0545

    layout and content of profile finished

--- src/components/ProfileContent.js
+++ src/components/ProfileContent.js
@@ -4,6 +4,15 @@ import profile from "../img/profile.jpg";
 import car_one from "../img/car_one.jpg";
 import car_two from "../img/car_two.jpg";
 import car_three from "../img/car_three.jpg";
+import { GrOverview } from "react-icons/gr";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { IoLogoInstagram } from "react-icons/io5";
+import { FaLongArrowAltRight } from "react-icons/fa";
+
+import { FaFile } from "react-icons/fa";
+import { FaPenFancy } from "react-icons/fa";
+
 const ProfileContent = () => {
   return (
     <div className="ps-5">
@@ -13,37 +22,50 @@ const ProfileContent = () => {
           <div className="d-flex gap-2 align-items-center ">
             <img src={profile} alt="" height="60px" className="rounded-3" />
             <div>
-              <h5 className="mb-0">Lily Smith</h5>
+              <h5 className="mb-0 fw-bolder">Lily Smith</h5>
               <span>mark@simmmple.com</span>
             </div>
           </div>
-          <div className="d-flex gap-3">
+          <div className="d-flex gap-3 align-items-center">
             <button type="" className="btn btn-primary">
-              OVERVIEW
+              <GrOverview />
+              <span className="fw-bolder"> OVERVIEW</span>
             </button>
             <button type="" className="btn text-light bg-transparent border-0">
-              TEAMS
+              <FaFile />
+              <span className="fw-bolder"> TEAMS</span>
             </button>
             <button type="" className="btn text-light bg-transparent border-0">
-              PROJECTS
+              <FaPenFancy /> <span className="fw-bolder"> PROJECTS</span>
             </button>
           </div>
         </div>
         <div className="col-6">
           <div className="card welcome_bg text-light">
             <div className="card-body">
-              <h3>Welcome Back!</h3>
-              <span className="d-inline-block pb-5">
-                Nice to see you, Mark Johnson!
-              </span>
-              <p className="pt-5">Tab to record </p>
+              <h3 className="fw-bolder">Welcome Back!</h3>
+              <div className="pb-4">
+                <span className="d-inline-block pb-5 mb-5">
+                  Nice to see you, Mark Johnson!
+                </span>
+              </div>
+              <div className="pt-3">
+                <p className="pt-5 mt-5">
+                  <button
+                    type=""
+                    className="border-0 outline-0 bg-transparent text-light"
+                  >
+                    Tab to record
+                  </button>
+                </p>
+              </div>
             </div>
           </div>
         </div>
         <div className="col-6">
           <div className="card profile-bg text-light">
             <div className="card-body">
-              <h5>Profile Information</h5>
+              <h5 className="fw-bolder pb-3">Profile Information</h5>
               <p>
                 Hi, I’m Mark Johnson, Decisions: If you can’t decide, the answer
                 is no. If two equally difficult paths, choose the one more
@@ -51,24 +73,28 @@ const ProfileContent = () => {
                 illusion of equality).
               </p>
               <p>
-                <span>Full Name:</span>
+                <span className="details_color">Full Name:</span>
                 <span>Mark Johnson</span>
               </p>
               <p>
-                <span>Mobile:</span>
+                <span className="details_color">Mobile:</span>
                 <span>(44) 123 1234 123</span>
               </p>
               <p>
-                <span>Email:</span>
+                <span className="details_color">Email:</span>
                 <span>mark@simmmple.com</span>
               </p>
               <p>
-                <span>Location:</span>
+                <span className="details_color">Location:</span>
                 <span>United States</span>
               </p>
-              <p>
-                <span>Social Media:</span>
-                <span>Facebook</span>
+              <p className="d-flex gap-2 align-items-center">
+                <span className="details_color">Social Media:</span>
+                <span className="d-inline-flex gap-2 ">
+                  <IoLogoInstagram />
+                  <FaFacebook />
+                  <FaTwitter />
+                </span>
               </p>
             </div>
           </div>
@@ -76,22 +102,33 @@ const ProfileContent = () => {
         <div className="col-12">
           <div className="card profile-bg text-light">
             <div className="card-body">
-              <h5>Car Information</h5>
-              <p>Hello, Mark Johnson Your car is ready.</p>
-              <div className="d-flex gap-4">
-                <div>
-                  <div>
-                    <h1>68%</h1>
-                    <p>Current Load</p>
-                  </div>
-                  <div>
-                    <h5>0h 58 min</h5>
-                    <p>Time to full charge</p>
+              <h5 className="fw-bolder">Car Informations</h5>
+              <p className="details_color">
+                Hello, Mark Johnson Your car is ready.
+              </p>
+              <div
+                className=""
+                style={{
+                  display: "grid",
+                  gridTemplateColumns: "15% 40% 40%",
+                  gridColumnGap: "3%",
+                }}
+              >
+                <div className="">
+                  <div className="w-100">
+                    <div className="bg-dark rounded-circle p-4 text-center">
+                      <h1>68%</h1>
+                      <p>Current Load</p>
+                    </div>
+                    <div className="text-center pt-3">
+                      <h5 className="fw-bolder mb-0">0h 58 min</h5>
+                      <p className="details_color">Time to full charge</p>
+                    </div>
                   </div>
                 </div>
-                <div>
-                  <div className="card bg-sidebar-color text-light">
-                    <div className="card-body">
+                <div className="w-100 d-flex flex-column gap-3">
+                  <div className="w-100 card bg-sidebar-color text-light">
+                    <div className="card-body d-flex justify-content-between align-items-center">
                       <div>
                         <p>Battery Health</p>
                         <h5>76%</h5>
@@ -100,7 +137,7 @@ const ProfileContent = () => {
                     </div>
                   </div>
                   <div className="card bg-sidebar-color text-light">
-                    <div className="card-body">
+                    <div className="card-body d-flex justify-content-between align-items-center">
                       <div>
                         <p>Consumtion</p>
                         <h5>163W/Km</h5>
@@ -109,17 +146,17 @@ const ProfileContent = () => {
                     </div>
                   </div>
                 </div>
-                <div>
-                  <div className="card bg-sidebar-color text-light">
+                <div className="w-100 d-flex flex-column gap-3">
+                  <div className="w-100 card bg-sidebar-color text-light">
                     <div className="card-body">
-                      <p>Efficiency</p>
-                      <h5>+20%</h5>
+                      <p className="date_style mb-3">Efficiency</p>
+                      <h5 className="fw-bold">+20%</h5>
                     </div>
                   </div>
                   <div className="card bg-sidebar-color text-light">
                     <div className="card-body">
-                      <p>This Week</p>
-                      <h5>1.342km</h5>
+                      <p className="date_style mb-3">This Week</p>
+                      <h5 className="fw-bold">1.342km</h5>
                     </div>
                   </div>
                 </div>
@@ -130,24 +167,117 @@ const ProfileContent = () => {
         <div className="col-3">
           <div className="card profile-bg text-light">
             <div className="card-body">
-              <h5>Platform Settings</h5>
-              <p>Account</p>
-              <p>Email me when someone....</p>
-              <p>Email me when someone....</p>
-              <p>Email me when someone....</p>
-              <p>Application</p>
-              <p>New Launched and projects</p>
-              <p>Month product changes</p>
-              <p>Subscribe to newsletter</p>
-              <p>Receive mails weekly</p>
+              <h5 className="fw-bold pb-3">Platform Settings</h5>
+              <p className="date_style">Account</p>
+              <div className="d-flex flex-column gap-4">
+                <div class="form-check form-switch">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    id="flexSwitchCheckChecked"
+                    checked
+                  />
+                  <label
+                    class="form-check-label date_style"
+                    for="flexSwitchCheckChecked"
+                  >
+                    Email me when someone....
+                  </label>
+                </div>
+                <div class="form-check form-switch">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    id="flexSwitchCheckChecked"
+                  />
+                  <label
+                    class="form-check-label date_style"
+                    for="flexSwitchCheckChecked"
+                  >
+                    Email me when someone....
+                  </label>
+                </div>
+                <div class="form-check form-switch">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    id="flexSwitchCheckChecked"
+                    checked
+                  />
+                  <label
+                    class="form-check-label date_style"
+                    for="flexSwitchCheckChecked"
+                  >
+                    Email me when someone....
+                  </label>
+                </div>
+              </div>
+
+              <p className="date_style pt-3">Application</p>
+              <div className="d-flex flex-column gap-4">
+                <div class="form-check form-switch">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    id="flexSwitchCheckChecked"
+                  />
+                  <label
+                    class="form-check-label date_style"
+                    for="flexSwitchCheckChecked"
+                  >
+                    New Launched and projects
+                  </label>
+                </div>
+                <div class="form-check form-switch">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    id="flexSwitchCheckChecked"
+                    checked
+                  />
+                  <label
+                    class="form-check-label date_style"
+                    for="flexSwitchCheckChecked"
+                  >
+                    Month product changes
+                  </label>
+                </div>
+                <div class="form-check form-switch">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    id="flexSwitchCheckChecked"
+                  />
+                  <label
+                    class="form-check-label date_style"
+                    for="flexSwitchCheckChecked"
+                  >
+                    Subscribe to newsletter
+                  </label>
+                </div>
+                <div class="form-check form-switch">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    id="flexSwitchCheckChecked"
+                    checked
+                  />
+                  <label
+                    class="form-check-label date_style"
+                    for="flexSwitchCheckChecked"
+                  >
+                    Receive mails weekly
+                  </label>
+                </div>
+              </div>
             </div>
           </div>
         </div>
         <div className="col-9">
           <div className="card profile-bg text-light">
             <div className="card-body">
-              <h5>Projects</h5>
-              <p>Architects design houses</p>
+              <h5 className="fw-bold">Projects</h5>
+              <p className="date_style">Architects design houses</p>
               <div className="d-flex gap-1">
                 <div className="card bg-sidebar-color text-light">
                   <div className="card-body">
@@ -157,9 +287,9 @@ const ProfileContent = () => {
                       width="100%"
                       className="rounded-3"
                     />
-                    <span>Project #1</span>
-                    <h5>Modern</h5>
-                    <p>
+                    <span className="project">Project #1</span>
+                    <h5 className="fw-bold">Modern</h5>
+                    <p className="date_style">
                       As Uber works through a huge amount of internal management
                       turmoil.
                     </p>
@@ -178,9 +308,9 @@ const ProfileContent = () => {
                       width="100%"
                       className="rounded-3"
                     />
-                    <span>Project #2</span>
-                    <h5>Scandinavian</h5>
-                    <p>
+                    <span className="project ">Project #2</span>
+                    <h5 className="fw-bold">Scandinavian</h5>
+                    <p className="date_style ">
                       Music is something that every person has his or her own
                       specific opinion about.
                     </p>
@@ -199,9 +329,9 @@ const ProfileContent = () => {
                       width="100%"
                       className="rounded-3"
                     />
-                    <span>Project #3</span>
-                    <h5>Minimalist</h5>
-                    <p>
+                    <span className="project">Project #3</span>
+                    <h5 className="fw-bold">Minimalist</h5>
+                    <p className="date_style">
                       Different people have different taste, especially various
                       types of music.
                     </p>
